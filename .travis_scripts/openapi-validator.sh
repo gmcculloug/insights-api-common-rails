7fb2916c270cae08f78c42afbc2bf20ead2d0de1
Greg McCullough <gmccullo@redhat.com>
2020-07-10 16:15:13 -0400

    Download openapi-generator script

--- .travis_scripts/openapi-validator.sh
+++ .travis_scripts/openapi-validator.sh
@@ -2,6 +2,9 @@
 set -eu
 set -o pipefail
 
+curl -L https://raw.githubusercontent.com/OpenAPITools/openapi-generator/master/bin/utils/openapi-generator-cli.sh > .travis_scripts/openapi-generator-cli
+chmod +x .travis_scripts/openapi-generator-cli
+
 if ! ver=$(./openapi-generator-cli version | tail -1); then
   ver="4.3.1"  
   echo "Use default version ${ver} to validate"
