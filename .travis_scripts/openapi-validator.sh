dce7d895e6cbeb3d40b4f160ac538e3cb92f6770
Greg McCullough <gmccullo@redhat.com>
2020-07-10 16:17:57 -0400

    Update openapi-validator.sh

--- .travis_scripts/openapi-validator.sh
+++ .travis_scripts/openapi-validator.sh
@@ -2,8 +2,8 @@
 set -eu
 set -o pipefail
 
-curl -L https://raw.githubusercontent.com/OpenAPITools/openapi-generator/master/bin/utils/openapi-generator-cli.sh > .travis_scripts/openapi-generator-cli
-chmod +x .travis_scripts/openapi-generator-cli
+curl -L https://raw.githubusercontent.com/OpenAPITools/openapi-generator/master/bin/utils/openapi-generator-cli.sh > ./openapi-generator-cli
+chmod +x ./openapi-generator-cli
 
 if ! ver=$(./openapi-generator-cli version | tail -1); then
   ver="4.3.1"  
